850f8a94b7fbc6c24386b9b1f84377b08e2a8b9e
HENRYPOL-PC\HENRYPOL <juan.collaguazo01@epn.edu.ec>
2019-10-22 18:14:28 -0500

    parte juan

--- src/info3/Ventanas/Yajaira.java
+++ src/info3/Ventanas/Yajaira.java
@@ -162,7 +162,7 @@ public class Yajaira extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Siguiente s=new Siguiente();
+        Juan s=new Juan();
         s.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
