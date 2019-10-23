1b69355723c36518f38ecbf5765354b11a029df7
chrispaul20191610 <56648687+chrispaul20191610@users.noreply.github.com>
2019-10-22 21:20:06 -0500

    acabe mi ventana

--- src/info3/Ventanas/christian.java
+++ src/info3/Ventanas/christian.java
@@ -42,6 +42,11 @@ public class christian extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("siguiente");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("nombre: Christian paul llumiquinga ayala");
 
@@ -94,6 +99,13 @@ public class christian extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+Yajaira l = new Yajaira ();
+l.setVisible(true);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
